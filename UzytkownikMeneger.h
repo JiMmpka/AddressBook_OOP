3f2ecc593146a0821bf77690efbe9579cbaafb7b
PSCode <jimmpka.github@gmail.com>
2024-01-16 09:50:24 +0100

    Divide KsiazkaAdresowa to smaller class

---
+++ UzytkownikMeneger.h
@@ -0,0 +1,28 @@
+#ifndef UZYTKOWNIKMENEGER_H
+#define UZYTKOWNIKMENEGER_H
+
+#include <iostream>
+#include <vector>
+#include <windows.h>
+#include <fstream>
+#include <sstream>
+
+#include "Uzytkownik.h"
+#include "PlikZUzytkownikami.h"
+
+using namespace std;
+
+class UzytkownikMeneger{
+    int idZalogowanegoUzytkownika;
+    vector <Uzytkownik> uzytkownicy;
+
+    Uzytkownik podajDaneNowegoUzytkownika();
+    int pobierzIdNowegoUzytkownika();
+    bool czyIstniejeLogin(string login);
+    PlikZUzytkownikami plikZUzytkownikami; // jezli okrzysta sie z obiektu w kilku miejscach to mozna go utworzyc w pliku nag³owkowym
+
+public:
+    void rejestracjaUzytkownika();
+    void wypiszWszystkichUzytkownikow();
+};
+#endif // UZYTKOWNIKMENEGER_H
